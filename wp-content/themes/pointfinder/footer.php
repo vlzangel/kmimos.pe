052fed7e831b79dd4a575c119c3ee9ea875d7a89
Dajan Medina <dmedina2150@gmail.com>
2017-11-03 16:36:19 -0400

    Modificacion footer para analytics

--- wp-content/themes/pointfinder/footer.php
+++ wp-content/themes/pointfinder/footer.php
@@ -133,7 +133,19 @@
         }
     });
 </script>
+<!-- Dajan -->
+    <!--[if lt IE 9]>
+        <script src='".get_home_url()."/wp-content/themes/pointfinder/js/html5shiv.js'></script>
+    <![endif]-->
+<script type='text/javascript'>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','".get_home_url().'/wp-content/plugins/kmimos/javascript/analytics.js'."','ga');
 
+    ga('create', 'UA-103343585-1', 'auto');
+    ga('send', 'pageview');
+</script><!-- Dajan -->
 <?php
 $datos = kmimos_get_info_syte();
 $HTML = "</div></div>
@@ -538,19 +550,7 @@ $HTML = "</div></div>
                     ";
                 }
 
-        $HTML .= "}); </script><!-- Dajan -->
-            <!--[if lt IE 9]>
-                <script src='".get_home_url()."/wp-content/themes/pointfinder/js/html5shiv.js'></script>
-            <![endif]-->
-            <script type='text/javascript'>
-                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                })(window,document,'script','".get_home_url().'/wp-content/plugins/kmimos/javascript/analytics.js'."','ga');
-
-                ga('create', 'UA-103343585-1', 'auto');
-                ga('send', 'pageview');
-            </script><!-- Dajan -->
+        $HTML .= "}); 
         ";
 
         if(  $_SESSION['admin_sub_login'] == 'YES' ){
