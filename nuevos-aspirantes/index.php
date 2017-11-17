bbc16b7ffc044388fe928acd34146cc615df433a
Dajan Medina <dmedina2150@gmail.com>
2017-11-17 16:08:16 -0400

    Agregando popup de nuevos-aspirantes

--- nuevos-aspirantes/index.php
+++ nuevos-aspirantes/index.php
@@ -1,6 +1,8 @@
+<?php include_once(dirname(__DIR__).'/wp-load.php'); ?>
 <!DOCTYPE html>
 <html> 
     <head>
+    	<?php wp_head(); ?>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title>Quiero ser cuidador</title>
@@ -25,7 +27,64 @@
 		  ga('send', 'pageview');
 
 		</script>
- 
+ 		<style type="text/css">
+			#PageSubscribe{position:relative; max-width: 700px;  margin: 0 auto;  padding: 25px;  top: 75px; border-radius: 20px;  background: #ba2287;  overflow: hidden;}
+			#PageSubscribe .exit{float: right; cursor: pointer;}
+			#PageSubscribe .section{ width: 50%; padding: 10px; float: left; font-size: 17px; text-align: left;}
+			#PageSubscribe .section.section1{font-size: 20px;}
+			#PageSubscribe .section.section1 span{font-size: 25px;}
+			#PageSubscribe .section.section1 .images{padding:10px 0; text-align: center;}
+			#PageSubscribe .section.section3{width: 100%; font-size: 17px; font-weight: bold; text-align: center;}
+			#PageSubscribe .section.section2{}
+			#PageSubscribe .section.section2 .message{font-size: 15px; border: none; background: none; opacity:0; visible: hidden; transition: all .3s;}
+			#PageSubscribe .section.section2 .message.show{opacity:1; visible:visible;}
+			#PageSubscribe .section.section2 .icon{width: 30px; padding: 5px 0;}
+			#PageSubscribe .section.section2 .subscribe {margin: 20px 0;  }
+			#PageSubscribe .section.section2 form{margin: 0; display:flex;}
+			#PageSubscribe .section.section2 input,
+			#PageSubscribe .section.section2 button{width: 100%; max-width: calc(100% - 60px); margin: 5px; padding: 5px 10px; color: #CCC; font-size: 15px; border-radius: 20px;  border: none; background: #FFF; }
+			#PageSubscribe .section.section2 button {padding: 10px;  width: 40px;}
+
+			@media screen and (max-width:480px), screen and (max-device-width:480px) {
+				#PageSubscribe { top: 15px;}
+				#PageSubscribe .section{ width: 100%; padding: 10px 0; font-size: 12px;}
+				#PageSubscribe .section.section1 {font-size: 15px;}
+				#PageSubscribe .section.section1 span {font-size: 20px;}
+				#PageSubscribe .section.section3 {font-size: 12px;}
+			}
+
+			.container-fluid {
+				padding-right: 0;
+				padding-left: 0;
+			}
+			.row {
+				margin-right: 0;
+				margin-left: 0;
+			}
+		</style>
+
+		<script type='text/javascript'>
+			//Subscribe
+			function SubscribeSite(){
+				clearTimeout(SubscribeTime);
+
+				var dog = '<img height="70" align="bottom" src="https://www.kmimos.com.mx/wp-content/uploads/2017/07/propuestas-banner-09.png">' +
+					'<img height="20" align="bottom" src="https://www.kmimos.com.mx/wp-content/uploads/2017/07/propuestas-banner-10.png">';
+
+				var html='<div id="PageSubscribe"><i class="exit fa fa-times" aria-hidden="true" onclick="SubscribePopUp_Close(\'#message.Msubscribe\')"></i>' +
+					'<div class="section section1"><span>G&aacute;nate <strong>S/.8</strong> en tu primera reserva</span><br>&#8216;&#8216;Aplica para clientes nuevos&#8217;&#8217;<div class="images">'+dog+'</div></div>' +
+					'<div class="section section2"><span><strong>&#161;SUSCR&Iacute;BETE!</strong> y recibe el Newsletter con nuestras <strong>PROMOCIONES, TIPS DE CUIDADOS PARA MASCOTAS,</strong> etc.!</span><?php echo subscribe_input('nuevos-aspirantes'); ?></div>';
+
+
+				SubscribePopUp_Create(html);
+			}
+
+			jQuery(document).ready(function(e){
+				SubscribeTime = setTimeout(function(){
+					SubscribeSite();
+				}, 7400);
+			});
+		</script>
 
     </head>
     <body>
